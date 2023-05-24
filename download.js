1dca7584cf31045d9c07e2d42eba89db6285ce8b
Lily Skye <me@suchipi.com>
2023-05-24 11:33:18 -0600

    allow specifying json file as command-line option

--- download.js
+++ download.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env node
+const path = require("path");
 const sh = require("shelljs");
-const genshinStickers = require("./genshin-stickers.json");
+
+let stickersPath = "./genshin-stickers.json";
+if (typeof process.argv[2] === "string" && process.argv[2].endsWith(".json")) {
+  stickersPath = process.argv[2];
+  if (!path.isAbsolute(stickersPath)) {
+    stickersPath = path.resolve(process.cwd(), stickersPath);
+  }
+}
+
+const stickers = require(stickersPath);
 
 sh.mkdir("-p", "stickers");
 
-const prefixDigits = String(genshinStickers.length).length;
+const prefixDigits = String(stickers.length).length;
 
 let i = -1;
-for (const { href, name } of genshinStickers) {
+for (const { href, name } of stickers) {
   i++;
   const filename = name.replace(/[^A-Za-z0-9 ]/g, "");
   const prefix = String(i).padStart(prefixDigits, "0");
